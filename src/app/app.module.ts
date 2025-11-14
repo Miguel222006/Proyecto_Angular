f4293693a97971beb4129ad651e4b631104de576
Miguel222006 <miguelangelpatinobuiles@gmail.com>
2025-11-14 14:42:29 -0500

    Proyecto contador en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { ActionButtonComponent } from './components/action-button/action-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ActionButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/action-button/action-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-action-button',
+  templateUrl: './action-button.component.html',
+  styleUrls: ['./action-button.component.scss']
+})
+export class ActionButtonComponent {
+  @Input() content: string = "";
+  @Input() color: string = ""
+
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter();
+
+  onClick() {
+    this.buttonClick.emit()
+  }
+}
